0ded95b0a8da84cca724d09089c347d9fd74f605
Svetlana Z <szakharova86@gmail.com>
2025-10-27 13:22:19 +0300

    Введение в HTTPX.Практическое задание: работа с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -24,6 +24,6 @@ access_token = login_response_data["token"]["accessToken"]
 user_response = httpx.get("http://localhost:8000/api/v1/users/me", headers={"Authorization": f"Bearer {access_token}"})
 user_response_data = user_response.json()
 
-# Выводим обновленные токены
+# Выводим данные пользователя
 print("Get user response:", user_response_data)
 print("Status Code:",user_response.status_code)
