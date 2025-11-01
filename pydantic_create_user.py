436cfab20a219995df347baf9f646ea02e34c045
Svetlana Z <szakharova86@gmail.com>
2025-11-01 13:38:57 +0300

    Знакомство с Pydantic.Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, SecretStr
+from pydantic import BaseModel, Field, SecretStr, EmailStr
 
 from tools.fakers import get_random_email
 
@@ -8,7 +8,7 @@ class UserSchema(BaseModel):
     Описание структуры пользователя.
     """
     id: str
-    email: str  # намеренно аннотируем как str, чтобы тестировать негативные кейсы
+    email: EmailStr
     last_name: str = Field(alias="lastName")
     first_name: str = Field(alias="firstName")
     middle_name: str = Field(alias="middleName")
@@ -17,7 +17,7 @@ class CreateUserRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание пользователя.
     """
-    email: str = Field(default_factory=get_random_email)
+    email: EmailStr = Field(default_factory=get_random_email)
     password: SecretStr ="12345678"
     last_name: str = Field(alias="lastName", default="Иванов")
     first_name: str = Field(alias="firstName", default="Иван")
