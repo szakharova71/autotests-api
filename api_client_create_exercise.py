5ca5c8d16e7e11a8f57bfd98c00edca21685ad5b
Svetlana Z <szakharova86@gmail.com>
2025-11-12 09:35:16 +0300

    Знакомство с Faker.Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -10,18 +10,12 @@ from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 # Вместо CreateUserRequestDict импортируем CreateUserRequestSchema
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    last_name="string",
-    first_name="string",
-    middle_name="string"
-)
+# Больше нет необходимости передавать значения, они будут генерировать автоматически
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -36,21 +30,14 @@ exercises_client= get_exercises_client(authentication_user)
 
 # Загружаем файл
 # Вместо CreateFileRequestDict используем CreateFileRequestSchema
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="./testdata/files/image.png"
-)
+# Автоматическая генерация данных, кроме необходимых параметров
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
+# Используем сгенерированные значения для создания курса
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    max_score=100,
-    min_score=10,
-    description="Python API course",
-    estimated_time="2 weeks",
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
 )
@@ -58,14 +45,7 @@ create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
 # Создаем задание
-create_exercise_request = CreateExerciseRequestSchema(
-    title="Новое задание",
-    course_id=create_course_response.course.id,
-    max_score=50,
-    min_score=5,
-    order_index=1,
-    description= "Описание нового задания",
-    estimated_time="2 часа",
-)
+# Используем сгенерированные значения для создания задания
+create_exercise_request = CreateExerciseRequestSchema(course_id=create_course_response.course.id)
 create_exercise_response=exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, Field, ConfigDict
 
 from clients.courses.courses_schema import CourseSchema
+# Импортируем заранее созданный экземпляр класса Fake
+from tools.fakers import fake
 
 # Добавили описание структуры задания
 class ExerciseSchema(BaseModel):
@@ -45,13 +47,20 @@ class CreateExerciseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str
-    course_id: str = Field(alias="courseId")
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    order_index: int = Field(alias="orderIndex")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
+    # Добавили генерацию случайного заголовка
+    title: str = Field(default_factory=fake.sentence)
+    # Добавили генерацию случайного идентификатора курса
+    course_id: str = Field(alias="courseId", default_factory=fake.uuid4)
+    # Добавили генерацию случайного максимального балла
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    # Добавили генерацию случайного минимального балла
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    # Добавили генерацию случайного индекса
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    # Добавили генерацию случайного описания
+    description: str = Field(default_factory=fake.text)
+    # Добавили генерацию случайного предполагаемого времени прохождения задания
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 # Добавили описание структуры ответа создания задания
 class CreateExerciseResponseSchema(BaseModel):
@@ -66,12 +75,19 @@ class UpdateExerciseRequestSchema(BaseModel):
     Описание структуры запроса на обновление задания.
     """
     model_config = ConfigDict(populate_by_name=True)
-    title: str | None
-    max_Score: int | None = Field(alias="maxScore")
-    min_score: int | None = Field(alias="minScore")
-    order_index: int | None = Field(alias="orderIndex")
-    description: str | None
-    estimated_time: str | None = Field(alias="estimatedTime")
+
+    # Добавили генерацию случайного заголовка
+    title: str | None = Field(default_factory=fake.sentence)
+    # Добавили генерацию случайного максимального балла
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    # Добавили генерацию случайного минимального балла
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    # Добавили генерацию случайного индекса
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.integer)
+    # Добавили генерацию случайного описания
+    description: str | None = Field(default_factory=fake.text)
+    # Добавили генерацию случайного предполагаемого времени прохождения задания
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 # Добавили описание структуры ответа обновления задания
 class UpdateExerciseResponseSchema(BaseModel):
@@ -79,3 +95,4 @@ class UpdateExerciseResponseSchema(BaseModel):
     Описание структуры ответа обновления задания.
     """
     exercise: ExerciseSchema
+
