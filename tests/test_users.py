ade17297a8b84ea72b5ca2873f211d7256a07beb
Svetlana Z <szakharova86@gmail.com>
2025-11-14 07:04:13 -0800

    Маркировки Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
-
+# Импортируем библиотеку pytest
+import pytest
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 # Импортируем функцию проверки статус-кода
@@ -9,7 +10,8 @@ from tools.assertions.schema import validate_json_schema
 # Импортируем функцию для проверки ответа создания юзера
 from tools.assertions.users import assert_create_user_response
 
-
+@pytest.mark.users  # Добавили маркировку users
+@pytest.mark.regression  # Добавили маркировку regression
 def test_create_user():
     # Инициализируем API-клиент для работы с пользователями
     public_users_client = get_public_users_client()
