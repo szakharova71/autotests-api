d48dbab96d14ee0e98bee2a91794652308d3114c
Svetlana Z <szakharova86@gmail.com>
2025-11-18 03:43:08 -0800

    Параметризация Pytest.Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -12,14 +12,19 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 # Импортируем функцию для проверки ответа создания юзера
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
+
 
 @pytest.mark.users  # Добавили маркировку users
 @pytest.mark.regression  # Добавили маркировку regression
-def test_create_user(public_users_client: PublicUsersClient): # Используем фикстуру API клиента
-    # Удалили инициализацию API клиента из теста
 
+@pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(domain: str, public_users_client: PublicUsersClient): # Используем фикстуру API клиента
+    # Удалили инициализацию API клиента из теста
+    # Формируем фейковый email с параметризованным доменом
+    email=fake.email(domain=domain)
     # Формируем тело запроса на создание пользователя
-    request = CreateUserRequestSchema()
+    request = CreateUserRequestSchema(email=email)
     # Отправляем запрос на создание пользователя
     response = public_users_client.create_user_api(request)
     # Инициализируем модель ответа на основе полученного JSON в ответе
