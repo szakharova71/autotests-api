ec03e5ab8908d9da403490c7b372454a38b06b71
Svetlana Z <szakharova86@gmail.com>
2025-11-18 22:37:36 -0800

    Рефакторинг и подготовка к написанию API автотестов.Практика реализации фикстур

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 pytest_plugins = (
     "fixtures.users",
     "fixtures.files",  # Добавляем фикстуры для работы с файлами
-    "fixtures.courses",  # Добавляем фикстуры для работы с курсами
+    "fixtures.courses",  # Добавляем фикстуры для работы с vbкурсами
+    "fixtures.exercises",  # Добавляем фикстуры для работы с заданиями
     "fixtures.authentication"
 )
