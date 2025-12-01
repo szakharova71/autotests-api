972018b597a60d148511d35a3ab0f7c150a55e66
szakharova71 <szakharova71@users.noreply.github.com>
2025-12-01 18:46:24 +0000

    deploy: 065b945edfadba7cde3a5f32399fad57be5132a1

--- conftest.py
+++
@@ -1,9 +0,0 @@
-pytest_plugins = (
-    "fixtures.users",
-    "fixtures.files",  # Добавляем фикстуры для работы с файлами
-    "fixtures.courses",  # Добавляем фикстуры для работы с курсами
-    "fixtures.exercises",  # Добавляем фикстуры для работы с заданиями
-    "fixtures.authentication",
-
-    "fixtures.allure"  # Добавили плагин
-)
--- fixtures/authentication.py
+++
@@ -1,9 +0,0 @@
-import pytest  # Импортируем pytest
-
-# Импортируем API клиенты
-from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
-
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def authentication_client() -> AuthenticationClient:  # Аннотируем возвращаемое фикстурой значение
-    # Создаем новый API клиент для работы с аутентификацией
-    return get_authentication_client()
